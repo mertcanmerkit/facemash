477deb1eecade8affd384b7035cdd94c873bfdcd
Mertcan Merkit <mertcanmerkit@gmail.com>
2021-11-30 11:20:30 +0300

    master 2

--- main.js
+++ main.js
@@ -1,3 +1,27 @@
-document.getElementById('button').addEventListener("click", function() {
-    alert("You clicked me");
-}​);​
+const header = document.getElementById("header");
+const scrollUp = "sticky-top";
+const scrollDown = "scroll-down";
+let lastScroll = 0;
+
+
+window.addEventListener("scroll", () => {
+    const currentScroll = window.pageYOffset;
+    if (currentScroll <= 0) {
+        header.classList.remove(scrollUp);
+        return;
+    }
+
+    if (currentScroll > lastScroll && !header.classList.contains(scrollDown)) {
+        // down
+        header.classList.remove(scrollUp);
+        header.classList.add(scrollDown);
+    } else if (
+        currentScroll < lastScroll &&
+        header.classList.contains(scrollDown)
+    ) {
+        // up
+        header.classList.remove(scrollDown);
+        header.classList.add(scrollUp);
+    }
+    lastScroll = currentScroll;
+});
