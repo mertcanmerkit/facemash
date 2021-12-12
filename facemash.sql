622223e3f33e2bd5ba26bccd94408be8970b294d
Osman Taha Kıyıcılar <tr.osmantaha@gmail.com>
2021-12-12 22:40:09 +0300

    update mysql table structre

--- facemash.sql
+++ facemash.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.4
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
--- Anamakine: localhost
--- Üretim Zamanı: 05 Ara 2021, 21:36:18
--- Sunucu sürümü: 10.4.17-MariaDB
--- PHP Sürümü: 8.0.2
+-- Anamakine: localhost:8889
+-- Üretim Zamanı: 12 Ara 2021, 19:39:43
+-- Sunucu sürümü: 5.7.34
+-- PHP Sürümü: 8.0.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -20,6 +20,70 @@ SET time_zone = "+00:00";
 --
 -- Veritabanı: `facemash`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `categories`
+--
+
+CREATE TABLE `categories` (
+  `id` int(11) NOT NULL,
+  `adder` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Tablo döküm verisi `categories`
+--
+
+INSERT INTO `categories` (`id`, `adder`, `name`) VALUES
+(1, 27, 'sarışınlar');
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `categoryData`
+--
+
+CREATE TABLE `categoryData` (
+  `id` int(11) NOT NULL,
+  `categoryId` int(11) NOT NULL,
+  `imageId` int(11) NOT NULL,
+  `count` int(11) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `categoryVotes`
+--
+
+CREATE TABLE `categoryVotes` (
+  `id` int(11) NOT NULL,
+  `categoryDataId` int(11) NOT NULL,
+  `userId` int(11) NOT NULL,
+  `votes` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `images`
+--
+
+CREATE TABLE `images` (
+  `id` int(11) NOT NULL,
+  `username` varchar(35) NOT NULL,
+  `adder` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Tablo döküm verisi `images`
+--
+
+INSERT INTO `images` (`id`, `username`, `adder`) VALUES
+(1, 'javaci', 27);
 
 -- --------------------------------------------------------
 
@@ -48,6 +112,34 @@ INSERT INTO `users` (`id`, `username`, `email`, `pass`, `token`, `ip`) VALUES
 --
 
 --
+-- Tablo için indeksler `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `category_user_id` (`adder`);
+
+--
+-- Tablo için indeksler `categoryData`
+--
+ALTER TABLE `categoryData`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `ctgry_data_id` (`categoryId`),
+  ADD KEY `ctgey_image_id` (`imageId`);
+
+--
+-- Tablo için indeksler `categoryVotes`
+--
+ALTER TABLE `categoryVotes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Tablo için indeksler `images`
+--
+ALTER TABLE `images`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`adder`);
+
+--
 -- Tablo için indeksler `users`
 --
 ALTER TABLE `users`
@@ -60,10 +152,57 @@ ALTER TABLE `users`
 --
 
 --
+-- Tablo için AUTO_INCREMENT değeri `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Tablo için AUTO_INCREMENT değeri `categoryData`
+--
+ALTER TABLE `categoryData`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Tablo için AUTO_INCREMENT değeri `categoryVotes`
+--
+ALTER TABLE `categoryVotes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Tablo için AUTO_INCREMENT değeri `images`
+--
+ALTER TABLE `images`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- Tablo için AUTO_INCREMENT değeri `users`
 --
 ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+
+--
+-- Dökümü yapılmış tablolar için kısıtlamalar
+--
+
+--
+-- Tablo kısıtlamaları `categories`
+--
+ALTER TABLE `categories`
+  ADD CONSTRAINT `category_user_id` FOREIGN KEY (`adder`) REFERENCES `users` (`id`);
+
+--
+-- Tablo kısıtlamaları `categoryData`
+--
+ALTER TABLE `categoryData`
+  ADD CONSTRAINT `ctgey_image_id` FOREIGN KEY (`imageId`) REFERENCES `images` (`id`),
+  ADD CONSTRAINT `ctgry_data_id` FOREIGN KEY (`categoryId`) REFERENCES `categories` (`id`);
+
+--
+-- Tablo kısıtlamaları `images`
+--
+ALTER TABLE `images`
+  ADD CONSTRAINT `user_id` FOREIGN KEY (`adder`) REFERENCES `users` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
