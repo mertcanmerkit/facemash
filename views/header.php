eea682fd6986f0f7ae26b60708082dc67565eb0d
Mertcan Merkit <mertcanmerkit@gmail.com>
2021-12-21 20:48:13 +0300

    mash modal ui updates. some ui updates.

--- views/header.php
+++ views/header.php
@@ -10,7 +10,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
     <link rel="stylesheet" href="views/css/style.css">
 
-    <title>Hello, world!</title>
+    <title>FaceMash</title>
 
 </head>
 
--- views/index.php
+++ views/index.php
@@ -1,14 +1,12 @@
 <div class="fixed-bottom mb-3">
     <div class="container">
-        <div class="row mt-3">
-            <div class="col">
+
                 <button type="button"
                         class="btn btn-outline-primary random-btn fw-bold d-flex justify-content-around align-items-center float-end"
                         data-bs-toggle="modal" data-bs-target="#exampleModal" id="random-btn">
                     <i class="fas fa-dice" style="font-size: 24px;"></i>
                 </button>
-            </div>
-        </div>
+
     </div>
 </div>
 
@@ -21,29 +19,51 @@
                 <h5 class="modal-title blue-text modalTitle"></h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
-            <div class="modal-body d-flex justify-content-center align-content-center align-items-center">
+            <div class="modal-body d-md-flex justify-content-center align-content-center align-items-center">
                 <div class="container">
                     <div class="row">
-                        <div class="col-md-6 mt-3 mb-3 d-flex justify-content-center align-content-center align-items-center">
-                            <div class="card align-items-center cursor-pointer" onclick="selectUser('first')">
+                        <div class="col-md-5 d-flex justify-content-md-end justify-content-center align-content-md-end align-content-center align-items-md-end align-items-center">
+                            <div class="card shadow align-items-center cursor-pointer" onclick="selectUser('first')">
                                 <input type="hidden" name="firstId">
                                 <img src="" class="card-img-top imageFirst"
                                      alt="">
                                 <div class="card-body">
-                                    <h5 class="card-title usernameFirst"></h5>
+                                    <h5 class="card-title usernameFirst text-break text-center"></h5>
+                                </div>
+                                <div class="progress">
+                                <span class="progress-bar d-flex" style="width: 25%">
+                                    <strong class="text-white" id="progress-voted">25%</strong>
+                                </span>
                                 </div>
                             </div>
                         </div>
-                        <div class="col-md-6 mt-3 mb-3 d-flex justify-content-center align-content-center align-items-center">
-                            <div class="card align-items-center" onclick="selectUser('second')">
+                        <div class="col-md-2 mt-2 mb-2 d-flex justify-content-center align-content-center align-items-center">
+                            <span class="text-white">O R</span>
+                        </div>
+                        <div class="col-md-5 d-flex justify-content-md-start justify-content-center align-content-md-start align-content-center align-items-md-start align-items-center">
+                            <div class="card shadow align-items-center" onclick="selectUser('second')">
                                 <input type="hidden" name="secondId">
                                 <img src="" class="card-img-top imageSecond"
                                      alt="">
                                 <div class="card-body">
-                                    <h5 class="card-title usernameSecond"></h5>
+                                    <h5 class="card-title usernameSecond text-break text-center"></h5>
+                                </div>
+                                <div class="progress">
+                                <span class="progress-bar progress-bar-not-selected d-flex" style="width: 75%">
+                                    <strong class="text-white" id="progress-voted">75%</strong>
+                                </span>
                                 </div>
                             </div>
                         </div>
+<!--                        <div class="row">-->
+<!--                            <div class="col mt-3 d-flex justify-content-center">-->
+<!--                                <div class="progress">-->
+<!--                                <span class="progress-bar d-flex" style="width: 25%">-->
+<!--                                    <strong class="text-white" id="progress-voted">25%</strong>-->
+<!--                                </span>-->
+<!--                                </div>-->
+<!--                            </div>-->
+<!--                        </div>-->
                     </div>
                 </div>
             </div>
